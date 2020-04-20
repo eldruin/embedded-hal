4fd349d8926cf9ccc971a47386fd5b7bba58133a
Diego Barrios Romero <eldruin@gmail.com>
2020-04-20 09:31:48 +0200

    Use interleaved buffer for data

--- src/blocking/i2s.rs
+++ src/blocking/i2s.rs
@@ -5,12 +5,10 @@ pub trait Read<W> {
     /// Error type
     type Error;
 
-    /// Reads enough bytes from the slave to fill `left_words` and `right_words`.
-    fn try_read<'w>(
-        &mut self,
-        left_words: &'w mut [W],
-        right_words: &'w mut [W],
-    ) -> Result<(), Self::Error>;
+    /// Reads enough bytes from the slave to fill `data`.
+    ///
+    /// The data is filled up with interleaved data as appropriate.
+    fn try_read<'w>(&mut self, data: &'w mut [W]) -> Result<(), Self::Error>;
 }
 
 /// Blocking write
@@ -18,12 +16,10 @@ pub trait Write<W> {
     /// Error type
     type Error;
 
-    /// Sends `left_words` and `right_words` to the slave.
-    fn try_write<'w>(
-        &mut self,
-        left_words: &'w [W],
-        right_words: &'w [W],
-    ) -> Result<(), Self::Error>;
+    /// Sends `data` to the slave.
+    ///
+    /// The data should be filled with interleaved data as appropriate.
+    fn try_write<'w>(&mut self, data: &'w [W]) -> Result<(), Self::Error>;
 }
 
 /// Blocking write (iterator version)
@@ -31,9 +27,10 @@ pub trait WriteIter<W> {
     /// Error type
     type Error;
 
-    /// Sends `left_words` and `right_words` to the slave.
-    fn try_write<LW, RW>(&mut self, left_words: LW, right_words: RW) -> Result<(), Self::Error>
+    /// Sends `data` to the slave.
+    ///
+    /// The data should be filled with interleaved data as appropriate.
+    fn try_write<I>(&mut self, data: I) -> Result<(), Self::Error>
     where
-        LW: IntoIterator<Item = W>,
-        RW: IntoIterator<Item = W>;
+        I: IntoIterator<Item = W>;
 }
--- src/i2s.rs
+++ src/i2s.rs
@@ -7,11 +7,9 @@ pub trait FullDuplex<Word> {
     /// Error type
     type Error;
 
-    /// Reads the left word and right word available.
-    ///
-    /// The order is in the result is `(left_word, right_word)`
-    fn try_read(&mut self) -> nb::Result<(Word, Word), Self::Error>;
+    /// Reads the word from the slave.
+    fn try_read(&mut self) -> nb::Result<Word, Self::Error>;
 
-    /// Sends a left word and a right word to the slave.
-    fn try_send(&mut self, left_word: Word, right_word: Word) -> nb::Result<(), Self::Error>;
+    /// Sends a word to the slave.
+    fn try_send(&mut self, word: Word) -> nb::Result<(), Self::Error>;
 }
